7eede9ea1c93cbad0eb3e0259e13d2de2951817f
eidanfili <eidan@ledezma.org>
2018-10-08 13:41:38 -0600

    built gradient component

---
+++ schedule/src/components/library/library.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Library extends Component {
+    render() {
+        return(
+            <div className="library">
+                <h2>Library</h2>
+            </div>
+        )    
+    }
+}
+export default Library;
---
+++ schedule/src/components/scheduler/shedule.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Gradient from '../gradient';
+
+class Schedule extends Component {
+    render() {
+        return(
+            <div className="schedule">
+                <h2>Schedule</h2>
+                <Gradient />
+            </div>
+        )    
+    }
+}
+export default Schedule;
