710882be2ab2f8335f855b15a26f59213c8f63db
eidanfili <eidan@ledezma.org>
2018-10-08 14:10:07 -0600

    built library course component

--- schedule/src/components/library/library.js
+++ schedule/src/components/library/library.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import LibraryCourse from './libraryCourse'
+
 class Library extends Component {
     render() {
         return(
             <div className="library">
                 <h2>Library</h2>
+                <LibraryCourse />
+                <LibraryCourse />
+                <LibraryCourse />
             </div>
         )    
     }
