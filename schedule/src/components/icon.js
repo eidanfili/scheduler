3d08d4685d8f379842d36a84a16ac42b800e2978
eidanfili <eidan@ledezma.org>
2018-10-08 14:44:34 -0600

    added icon component

---
+++ schedule/src/components/icon.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Icon = (icon, className) => {
+    return(
+        <div className="icon">
+            <i className={`${icon} ${className}`}></i>
+        </div>
+    )
+}
+export default Icon;
