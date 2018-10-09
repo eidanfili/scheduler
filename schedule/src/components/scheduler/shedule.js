f540ad1453535e79ef9d2e5f085df447fc347b9f
eidanfili <eidan@ledezma.org>
2018-10-09 16:29:52 -0600

    mapped course data ove to schedule component

--- schedule/src/components/scheduler/shedule.js
+++ schedule/src/components/scheduler/shedule.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import Gradient from '../gradient';
 import ScheduleCourse from './scheduleCourse';
 import ProgressTracker from './progresstracker'
 
 class Schedule extends Component {
+
+    renderCourses() {
+        const data = this.props.courses
+        
+        return data.map((course, index) => {
+            if (course.enrolled){
+            return <ScheduleCourse key={index} {...course} />
+            }
+        })
+    }
     render() {
         return(
             <div className="schedule">
                 <h2>Schedule</h2>
-                <ScheduleCourse title={'Problem Solving'}/>
-                <ScheduleCourse title={'Problem Solving'}/>
-                <ScheduleCourse title={'Problem Solving'}/>
+                {this.renderCourses}
                 <ProgressTracker/>
                 <Gradient />
             </div>
         )    
     }
 }
-export default Schedule;
+
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps)(Schedule);
